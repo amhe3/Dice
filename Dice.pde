9ce192a57a44b5ac39620ab2e286db74f45b5420
unknown <amhe3@697-334-Theoden.sf.k12.internal>
2015-09-11 10:21:21 -0700

    adding colors for dice

--- Dice.pde
+++ Dice.pde
@@ -1,5 +1,7 @@
+// http://www.rapidtables.com/web/color/RGB_Color.htm
 
 int num = 0;
+int colors = color(255); //background color of dice
 
 void setup()
 {
@@ -15,16 +17,17 @@ void draw()
 		for(int b = 25; b < 400; b+=125)
 		{
 			Die one = new Die(a, b);
+			fill(colors);
 			one.show();
+			fill(0);
 			one.roll();
 			fill(0);
-			rect(0, 390, 400, 40);
+			rect(0, 390, 400, 40); //cover bottom to prevent overlap
 			fill(255);
-			textAlign(CENTER,CENTER);
 			textSize(30);
-			text("Total: " + num , 100, 405); //show the sum
-			int averages = num/9;
-			text("Average: " + averages , 300, 405); //show the average
+			text("Total: " + num , 30, 415); //show the sum
+			float averages = num/9.0;
+			text("Average: " + averages , 195, 415); //show the average
 		}
 	}
 }
@@ -47,17 +50,20 @@ class Die //models one single dice cube
 	{
 		if(value == 1)
 		{
+			colors = color(255, 204, 204); //red
 			ellipse(myX+50, myY+50, 15, 15); //center point
 			num++; //for total
 		}
 		if(value == 2)
 		{
+			colors = color(255, 229, 204); //orange
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			num+=2; //for total
 		}
 		if(value == 3)
 		{
+			colors = color(255, 255, 204); //yellow
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+50, myY+50, 15, 15); //middle point
@@ -65,6 +71,7 @@ class Die //models one single dice cube
 		}
 		if(value == 4)
 		{
+			colors = color(229, 255, 204); //green
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+25, myY+75, 15, 15); //top rith
@@ -73,6 +80,7 @@ class Die //models one single dice cube
 		}
 		if(value == 5)
 		{
+			colors = color(240, 255, 255); //blue.... HERE!!!
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+25, myY+75, 15, 15); //top rith
@@ -82,6 +90,7 @@ class Die //models one single dice cube
 		}
 		if(value == 6)
 		{
+			colors = color(230, 230, 250);
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+25, myY+75, 15, 15); //top rith
@@ -94,8 +103,6 @@ class Die //models one single dice cube
 	void show()
 	{
 		strokeWeight(1);
-		fill(255);
 		rect(myX, myY, 100, 100);
-		fill(0);
 	}
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -16,6 +16,7 @@ public class Dice extends PApplet {
 
 
 int num = 0;
+int colors = color(255); //background color of dice
 
 public void setup()
 {
@@ -31,16 +32,17 @@ public void draw()
 		for(int b = 25; b < 400; b+=125)
 		{
 			Die one = new Die(a, b);
+			fill(colors);
 			one.show();
+			fill(0);
 			one.roll();
 			fill(0);
-			rect(0, 390, 400, 40);
+			rect(0, 390, 400, 40); //cover bottom to prevent overlap
 			fill(255);
-			textAlign(CENTER,CENTER);
 			textSize(30);
-			text("Total: " + num , 100, 405); //show the sum
-			int averages = num/9;
-			text("Average: " + averages , 300, 405); //show the average
+			text("Total: " + num , 30, 415); //show the sum
+			float averages = num/9.0f;
+			text("Average: " + averages , 195, 415); //show the average
 		}
 	}
 }
@@ -63,17 +65,20 @@ class Die //models one single dice cube
 	{
 		if(value == 1)
 		{
+			colors = color(255, 228, 225);
 			ellipse(myX+50, myY+50, 15, 15); //center point
 			num++; //for total
 		}
 		if(value == 2)
 		{
+			colors = color(255, 222, 173);
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			num+=2; //for total
 		}
 		if(value == 3)
 		{
+			colors = color(255, 245, 238);
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+50, myY+50, 15, 15); //middle point
@@ -81,6 +86,7 @@ class Die //models one single dice cube
 		}
 		if(value == 4)
 		{
+			colors = color(244, 238, 224);
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+25, myY+75, 15, 15); //top rith
@@ -89,6 +95,7 @@ class Die //models one single dice cube
 		}
 		if(value == 5)
 		{
+			colors = color(240, 255, 255);
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+25, myY+75, 15, 15); //top rith
@@ -98,6 +105,7 @@ class Die //models one single dice cube
 		}
 		if(value == 6)
 		{
+			colors = color(230, 230, 250);
 			ellipse(myX+25, myY+25, 15, 15); //top left point
 			ellipse(myX+75, myY+75, 15, 15); //bottom right point
 			ellipse(myX+25, myY+75, 15, 15); //top rith
@@ -110,9 +118,7 @@ class Die //models one single dice cube
 	public void show()
 	{
 		strokeWeight(1);
-		fill(255);
 		rect(myX, myY, 100, 100);
-		fill(0);
 	}
 }
   static public void main(String[] passedArgs) {
